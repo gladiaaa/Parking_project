5f2969295c10b3fc3d6f8406cc95488a344e8828
gladiaaa <annic.ryan@hotmail.com>
2025-10-31 13:12:56 +0100

    add Parking class

---
+++ Backend/src/parking.php
@@ -0,0 +1,87 @@
+<?php
+
+class Parking
+{
+    private string $gps;
+    private float $tarif;
+    private DateTime $HeureDebut;
+    private DateTime $HeureFin;
+    private array $list_reservation;
+    private array $list_stationnement;
+
+    public function __construct(
+        string $gps,
+        float $tarif,
+        DateTime $HeureDebut,
+        DateTime $HeureFin,
+        array $list_stationnement,
+        array $list_reservation = []
+    ) {
+        $this->gps = $gps;
+        $this->tarif = $tarif;
+        $this->HeureDebut = $HeureDebut;
+        $this->HeureFin = $HeureFin;
+        $this->list_reservation = $list_reservation;
+        $this->list_stationnement = $list_stationnement;
+    }
+
+    public function getHeureFin(): DateTime
+    {
+        return $this->HeureFin;
+    }
+
+    public function setHeureFin(DateTime $HeureFin): void
+    {
+        $this->HeureFin = $HeureFin;
+    }
+
+    public function getHeureDebut(): DateTime
+    {
+        return $this->HeureDebut;
+    }
+
+    public function setHeureDebut(DateTime $HeureDebut): void
+    {
+        $this->HeureDebut = $HeureDebut;
+    }
+
+    public function getTarif(): float
+    {
+        return $this->tarif;
+    }
+
+    public function setTarif(float $tarif): void
+    {
+        $this->tarif = $tarif;
+    }
+
+    public function getGps(): string
+    {
+        return $this->gps;
+    }
+
+    public function setGps(string $gps): void
+    {
+        $this->gps = $gps;
+    }
+
+    public function getListReservation(): array
+    {
+        return $this->list_reservation;
+    }
+
+    public function setListReservation(array $list_reservation): void
+    {
+        $this->list_reservation = $list_reservation;
+    }
+
+    public function getListStationnement(): array
+    {
+        return $this->list_stationnement;
+    }
+
+    public function setListStationnement(array $list_stationnement): void
+    {
+        $this->list_stationnement = $list_stationnement;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ProjecT_Parc
 Gestionnaire de places de parking
+
+
+## Création de la bdd :
+
+mysql -u root -h 127.0.0.1 -P 3306 < sql/001_init_core.sql
+
