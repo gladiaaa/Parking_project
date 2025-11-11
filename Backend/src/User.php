f0cf677b9e754a270228784af06add34e83d1063
bnjjs1998 <b_jaques@hetic.eu>
2025-11-11 19:03:27 +0100

    Creation de la classe User.php

--- Backend/src/User.php
+++ Backend/src/User.php
@@ -1,6 +1,68 @@
 <?php
 class User
 {
+    public string $email;
+    public string $password;
+    public string $name;
+    public string $prenoms;
+    public array $list_reservation;
+    public array $list_parking;
 
+    // Constructeur (appelé à la création d’un objet)
+    public function __construct(
+        string $email,
+        string $password,
+        string $name,
+        string $prenoms,
+        array $list_reservation = [],
+        array $list_parking = []
+    ) {
+        $this->email = $email;
+        $this->password = $password;
+        $this->name = $name;
+        $this->prenoms = $prenoms;
+        $this->list_reservation = $list_reservation;
+        $this->list_parking = $list_parking;
+    }
 
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPrenoms(): string
+    {
+        return $this->prenoms;
+    }
+
+    public function setPrenoms(string $prenoms): void
+    {
+        $this->prenoms = $prenoms;
+    }
 }
+?>
